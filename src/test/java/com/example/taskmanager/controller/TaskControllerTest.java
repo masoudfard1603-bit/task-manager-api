65f2d42cbbedb8392302ba527591cc7f8b5ac134
masoudfard1603-bit <masoudfard1603@gmail.com>
2026-07-05 18:21:23 +0330

    Fix TaskControllerTest missing security beans

--- src/test/java/com/example/taskmanager/controller/TaskControllerTest.java
+++ src/test/java/com/example/taskmanager/controller/TaskControllerTest.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import com.example.taskmanager.security.JwtService;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -34,6 +36,12 @@ class TaskControllerTest {
 
     @MockBean
     private TaskService taskService;
+
+    @MockBean
+    private JwtService jwtService;
+
+    @MockBean
+    private UserDetailsService userDetailsService;
 
     @Test
     @WithMockUser(username = "alice", roles = "USER")
