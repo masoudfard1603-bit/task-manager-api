0f5f7bb5ab5cb1826951830779922efdb3e6620f
masoudfard1603-bit <masoudfard1603@gmail.com>
2026-07-07 02:18:19 +0330

    Fix integration test datasource driver

--- src/test/java/com/example/taskmanager/integration/TaskManagerIntegrationTest.java
+++ src/test/java/com/example/taskmanager/integration/TaskManagerIntegrationTest.java
@@ -40,6 +40,7 @@ class TaskManagerIntegrationTest {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
     }
 
     @Autowired
